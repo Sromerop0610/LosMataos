5a122eb4676a2d0b74875e31d3a9d7b0b9e897e2
sdurutr436 <sdurutr436@g.educaand.es>
2026-01-31 00:13:42 +0100

    Creacion carta base y funciones básicas de mano

---
+++ Scripts/Objects/_carta.gd
@@ -0,0 +1,11 @@
+class_name carta_base
+extends Node2D
+
+@export var id: String
+@export var title: String
+@export var puntos_letras: int
+@export var componentes: int
+@export var afinacion: int
+
+func play(target: Node) -> void:
+	pass
