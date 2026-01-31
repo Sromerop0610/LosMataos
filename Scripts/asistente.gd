ed8014a69495f575bd3e07f3617355149d0b374a
oscargarcia <ogarjae565@g.educaand.es>
2026-01-31 12:33:46 +0100

    Animaciones

---
+++ Scripts/asistente.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+@export var escala = 1.7
+@export var salto = .3
+@export var caida = .15
+
+func activar_salto():
+	# para que no salten todos a la vez
+	$Timer.wait_time += randf_range(-0.18, 0.18)
+	$Timer.start()
+
+func _on_timer_timeout() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1,escala), salto).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "scale", Vector2(1,1), caida).set_trans(Tween.TRANS_BOUNCE)
