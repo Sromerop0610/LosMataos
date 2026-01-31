197fdf26a13b8fb8538fe46c1e32b734c6056d8c
javijjah <javijjah@gmail.com>
2026-01-31 16:57:13 +0100

    credits added

--- Scripts/menú_principal.gd
+++ Scripts/menú_principal.gd
@@ -1,20 +1,27 @@
 extends Control
 
-@onready var play_button = $VBoxContainer/PlayButton
-@onready var options_button = $VBoxContainer/OptionsButton
-@onready var quit_button = $VBoxContainer/QuitButton
+@onready var play_button = $BotonesMenu/PlayButton
+@onready var options_button = $BotonesMenu/OptionsButton
+@onready var quit_button = $BotonesMenu/QuitButton
+@onready var volver = $Creditos/Volver
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@onready var creditos = $Creditos
+@onready var botones_menu = $BotonesMenu
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 
-
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+func _ready():
+	if OS.get_name() = "Web":
+		quit_button.hide()
+
+func _on_volver_pressed():
+	creditos.hide()
+	botones_menu.show()
+
+func _on_creditos_button_pressed():
+	botones_menu.hide()
+	creditos.show()
