af2e1948e11cb1decd0d6c6a01aff5ff050df136
Víctor <vgomtej353@g.educaand.es>
2026-01-31 16:05:20 +0100

    Scripts inventario y mascara

---
+++ Scripts/Objects/inventario.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var inventario: Array = []
+var objetoMascara: mascara
+
+func llenarInventario():
+	inventario.append(objetoMascara)
---
+++ Scripts/Objects/mascara.gd
@@ -0,0 +1,7 @@
+class_name mascara
+
+@export var id: String
+@export var nombreMascara: String
+@export var puntosLetras: int
+@export var puntosComponentes: int
+@export var puntosAfinacion: int
