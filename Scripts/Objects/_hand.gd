b33d6b7bf0947e801e241c74dd6e50feab4073ee
sdurutr436 <sdurutr436@g.educaand.es>
2026-01-31 11:36:27 +0100

    Aplicar cambios del stash (trash)

--- Scripts/Objects/_hand.gd
+++ Scripts/Objects/_hand.gd
@@ -4,6 +4,15 @@ extends Node2D
 
 const MANO_MAXIMA: int = 3
 
+func robar_carta():
+	while mano.size() < MANO_MAXIMA:
+		mano.append(carta_base)
+	return mano
+
+func flush_mano():
+	mano = []
+	return mano
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
