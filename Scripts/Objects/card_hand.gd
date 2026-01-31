640fda74e0cb0458e3ee339488ad556c54712334
sdurutr436 <sdurutr436@g.educaand.es>
2026-01-31 14:23:41 +0100

    terminada lógica de gestion de la mano: creación de cartas aleatoria (máscara a su evolución), sistema de robo para el array de la mano y flush de la mano al finalizar elección

--- Scripts/Objects/card_hand.gd
+++ Scripts/Objects/card_hand.gd
@@ -3,15 +3,26 @@ extends Node2D
 const MANO_MAXIMA: int = 3
 const CARTAS_POOL := card_pool.CARTAS
 
+# Precargar la escena de la carta
+const carta_scene = preload("res://Scenes/carta.tscn")
+
 var mano: Array = []
+
+func crear_carta_aleatoria() -> carta_base:
+	var data: Dictionary = CARTAS_POOL.pick_random()
+	var carta = carta_scene.instantiate()
+	carta.crear_carta(data)
+	return carta
 
 func robar_carta():
 	while mano.size() < MANO_MAXIMA:
-		mano.append(carta_base)
+		var carta := crear_carta_aleatoria()  # también falta esta lógica
+		mano.append(carta)
 	return mano
 
-func flush_mano():
-	mano = []
+func flush_mano(mano):
+	for carta in mano:
+		carta.queue_free()
 	return mano
 
 # Called when the node enters the scene tree for the first time.
